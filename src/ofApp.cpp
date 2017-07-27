c13611c1ed7de09898ea632d3f1118af0bb861d3
Andrew McWilliams <andy@jahya.net>
2017-07-26 22:22:36 -0400

    Toggles every half second between two images

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,70 +2,43 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    tex.setup("wedding.jpg");
+    masker.setup(2);
+    maskOn = true;
+
+    for(int i = 1; i <= 5; i++){
+        texture.setup("wedding" + ofToString(i) + ".jpg");
+        textures.push_back(texture);
+    }
+    masker.toggleOverlay();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    if(ofGetFrameNum() % 30 == 0){
+        maskOn = !maskOn;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    tex.draw();
-}
+    masker.beginLayer(0);
+    {
+        textures.at(0).draw();
+    }
+    masker.endLayer(0);
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+    masker.beginLayer(1);
+    {
+        textures.at(1).draw();
+    }
+    masker.endLayer(1);
 
-}
+    masker.beginMask(1);
+    {
+        maskOn ? ofBackground(ofColor::white) : ofBackground(ofColor::black);
+    }
+    masker.endMask(1);
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    masker.draw();
+    masker.drawOverlay();
 }
